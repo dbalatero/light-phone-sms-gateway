1c21404c06426c6c4a43b9a1ca4450a434616226
David Balatero <dbalatero@stripe.com>
2020-01-12 10:30:06 -0800

    First commit

---
+++ Gemfile
@@ -0,0 +1,4 @@
+# frozen_string_literal: true
+
+ruby '2.6.3'
+source 'https://rubygems.org'
---
+++ app.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+require 'rubygems'
+require 'bundler/setup'
+
+puts 'ok'
